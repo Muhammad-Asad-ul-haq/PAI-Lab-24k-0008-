a56bc66a013956e67ad5a5ef9d8ea200f62bd764
Muhammad-Asad-ul-haq <asadhaq01@gmail.com>
2025-09-11 01:32:45 +0500

    Q1.py

--- Lab 2/Q1.py
+++ Lab 2/Q1.py
@@ -1,1 +1,21 @@
+password = input("Enter your password: ")
 
+if len(password) >= 8:
+    letter = False
+    digit = False
+    special = False
+
+    for c in password:
+        if c.isalpha():
+            letter = True
+        elif c.isdigit():
+            digit = True
+        elif c in "@$%&":
+            special = True
+
+    if letter== True and digit== True and special==True:
+        print("Strong password")
+    else:
+        print("Password is not strong")
+else:
+    print("Password must be at least 8 characters.")
