f2e31e0d8492adb73782876b46ebc382625b09aa
Muhammad-Asad-ul-haq <asadhaq01@gmail.com>
2025-09-12 22:58:49 +0500

    Q1.py

---
+++ Lab 3/Q1.py
@@ -0,0 +1,9 @@
+def Calc_area(width,length):
+    z=width*length
+    print("The area of the rectangle is:",z)
+    return z
+
+x=float(input("Enter your length: "))
+y=float(input("Enter your width: "))
+
+Calc_area(x,y)
