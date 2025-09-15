8eb969f644e426883257448f7ec55c561f2a92fc
Muhammad-Asad-ul-haq <asadhaq01@gmail.com>
2025-09-15 18:26:36 +0500

    Q1.py

--- Lab 1/Q1.py
+++ Lab 1/Q1.py
@@ -1,12 +1,12 @@
-
-n = int(input("Enter a number: "))
-f = float(n)
-s = str(n)
-c = complex(n)
-print(type(f))
-print(type(s))
-print(type(c))
-if (n // 2) * 2 == n:
+x = input("Enter number: ")
+x = int(x)
+a = float(x)
+b = str(x)
+d = complex(x)
+print(type(a))
+print(type(b))
+print(type(d))
+if x - (x // 2) * 2 == 0:
     print("Divisible by 2")
 else:
     print("Not divisible by 2")
