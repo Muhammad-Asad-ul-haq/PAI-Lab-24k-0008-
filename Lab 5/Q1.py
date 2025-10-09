3b66b2787a0053bbf768620c67a87539f63ccdc7
Muhammad-Asad-ul-haq <asadhaq01@gmail.com>
2025-10-10 00:36:48 +0500

    Q1.py

--- Lab 5/Q1.py
+++ Lab 5/Q1.py
@@ -1,1 +1,13 @@
+try:
+    filename = input("Enter file name: ")
+    with open(filename, 'r') as f:
+        text = f.read()
+    chars = len(text)
+    words = len(text.split())
+    print("Characters:", chars)
+    print("Words:", words)
+except FileNotFoundError:
+    print("File not found.")
+except Exception as e:
+    print("Error:", e)
 
